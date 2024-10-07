f930141cee5cf8417466b7344dc6938ee6b80984
fflopsi <florian.l.frauenfelder@gmail.com>
2024-10-07 16:20:05 +0200

    Replace venvShellHook with own shellHook

--- shell.nix
+++ shell.nix
@@ -3,17 +3,26 @@ let
 in pkgs.mkShell {
   venvDir = "venv";
   packages = [ # buildInputs?
-    pkgs.python3.pkgs.venvShellHook
-    pkgs.python3
+    #pkgs.python3.pkgs.venvShellHook # replaced by shellHook below
+    #pkgs.python3
     (pkgs.python3.withPackages (python-pkgs: [
       #python-pkgs.pip
       python-pkgs.jupyter
       python-pkgs.numpy
       python-pkgs.scipy
+      python-pkgs.sympy
       python-pkgs.pandas
       python-pkgs.matplotlib
       python-pkgs.scikit-learn
       python-pkgs.scikit-image
     ]))
   ];
+  # remove old $venvDir if present
+  shellHook = ''
+    if [ -d "$venvDir" ]; then
+      rm -rf "$venvDir"
+    fi
+    python -m venv "$venvDir"
+    source "$venvDir/bin/activate"
+  '';
 }
